2120baf59fbe8d09eff26069fc6a3bc9c6235053
Aarushi <aarushisinha2005@gmail.com>
2024-04-09 11:10:19 +0530

    updates

---
+++ index.js
@@ -0,0 +1,25 @@
+const TEXT_1 = "The\nFuture\nTechnology\nWelcomes\n"
+const TEXT_2 = "You!"
+
+const typeElement_1 = document.getElementById("typing_1")
+const typeElement_2 = document.getElementById("typing_2")
+
+const eachLetterTime = 100
+
+for (let i = 0; i < TEXT_1.length; i++) {
+    setTimeout(()=>{
+        typeElement_1.innerText += TEXT_1[i]
+    },eachLetterTime*i)
+}
+
+setTimeout(()=>{
+    for (let i = 0; i < TEXT_2.length; i++) {
+        setTimeout(()=>{
+            typeElement_2.innerText += TEXT_2[i]
+        },eachLetterTime*i)
+    }     
+},(TEXT_1.length+1)*eachLetterTime)
+
+setTimeout(()=>{
+    typeElement_2.classList.add("glow")    
+},(TEXT_1.length+TEXT_2.length +5)*eachLetterTime)
